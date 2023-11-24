140efc9915754ecf8909056aceb6da1dfb82dd67
Shakhram <shakag6@gmail.com>
2023-11-24 15:46:12 -0500

    Create Home.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
 import { About } from './components/About';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <About/>
     </div>
   );
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,16 @@
+import "../styles/Home.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>
+        <span>Hi I am </span>
+        <span>
+          <span>Shakhram Gafurov</span>
+          <span class="cursor-blink">|</span>
+        </span>
+      </h1>
+      <p>More Info...</p>
+    </div>
+  );
+};
