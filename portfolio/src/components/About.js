f694af9773d56a72076b48903eae4d6f95b61a54
Shakhram <shakag6@gmail.com>
2023-11-25 13:49:00 -0500

    Add some info to About.js

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,3 +1,23 @@
+import "../styles/About.css"
+
 export const About = () => {
-  return <div>About... Skills... Most used languages...</div>;
+  return (
+    <div className="about-me-section">
+      <h2>About Me</h2>
+      <br/>
+      <p>SHAKHRAM GAFUROV</p>
+      <p>Brooklyn, New York</p>
+      <p>Brooklyn College, City University of New York</p>
+      <p>Bachelor of Science, Computer Science</p>
+      <p>● GPA: 3.7</p>
+      <p>
+        ● Relevant Courses: Data Structures, Algorithms, Java, Modern
+        Programming Techniques, Python, Operating Systems
+      </p>
+      <br/>
+      <p>hobbies</p>
+      <br/>
+      Skills... Most used languages...
+    </div>
+  );
 };
