385a286edc4e379554fe3af41e524c5fa4687de9
Shakhram <shakag6@gmail.com>
2023-11-29 23:10:06 -0500

    Add and edit images in About.js

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,9 +1,10 @@
 import React from "react";
 import figma from "../images/logos/figma.png";
 import firebase from "../images/logos/firebase.png";
-import git from "../images/logos/git.png";
+import githubLogo from "../images/githubLogo.png";
 import mongoDb from "../images/logos/mongodb.png";
 import netlify from "../images/logos/netlify.png";
+import postgres from "../images/logos/postgres.png";
 import "../styles/About.css";
 
 export const About = () => {
@@ -16,12 +17,14 @@ export const About = () => {
         <p>🎓 Brooklyn College, City University of New York</p>
         <p>📜 Bachelor of Science, Computer Science</p>
       </div>
+      <br />
       <h2>Hobbies</h2>
       <div>
         <h1>🥊 ☕ 🛹 📷 🥘</h1>
       </div>
+      <br />
       <h2>Languages / Frameworks</h2>
-      <div>
+      <div style={{ margin: "20px" }}>
         <img
           alt="javascript"
           src="https://camo.githubusercontent.com/aeddc848275a1ffce386dc81c04541654ca07b2c43bbb8ad251085c962672aea/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6a6176617363726970742d2532333332333333302e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d253233463744463145"
@@ -43,17 +46,15 @@ export const About = () => {
           src="https://camo.githubusercontent.com/27250b9f428b32314f8610e1a996939cc116da5f8c4d8a2f8ed37104275085b8/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f507974686f6e2d3134333534433f7374796c653d666f722d7468652d6261646765266c6f676f3d707974686f6e266c6f676f436f6c6f723d7768697465"
         />
       </div>
+      <br />
       <h2>Technologies</h2>
-      <div>
-        <img
-          alt="postgres"
-          src="https://camo.githubusercontent.com/29e7fc6c62f61f432d3852fbfa4190ff07f397ca3bde27a8196bcd5beae3ff77/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f706f7374677265732d2532333331363139322e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d706f737467726573716c266c6f676f436f6c6f723d7768697465"
-        />
-        <img src={figma} />
-        <img src={firebase} />
-        <img src={git} />
-        <img src={mongoDb} />
-        <img src={netlify} />
+      <div style={{ margin: "20px" }}>
+        <img className="technology-image" alt="postgres" src={postgres} />
+        <img className="technology-image" src={figma} />
+        <img className="technology-image" src={firebase} />
+        <img className="technology-image" src={githubLogo} />
+        <img className="technology-image" src={mongoDb} />
+        <img className="technology-image" src={netlify} />
       </div>
     </div>
   );
