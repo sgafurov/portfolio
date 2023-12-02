dfd6f48b54157c35d7c128d07df068fa173a0048
Shakhram <shakag6@gmail.com>
2023-12-01 23:18:57 -0500

    Add more to resume

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import githubLogo from "../images/logos/githubLogo.png";
 import linkedInLogoWhite from "../images/logos/linkedInLogoWhite.png";
-import movingProfileIcon from "../images/movingProfileIcon.mp4";
+import movingProfileIcon from "../images/movingProfileIcon1.5.mp4";
 import "../styles/Home.css";
 
 export const Home = () => {
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -4,7 +4,7 @@ export const Resume = () => {
   return (
     <div className="resume">
       <ol>
-        <li>
+        <li className="job-experience">
           <div
             style={{
               display: "flex",
@@ -19,14 +19,50 @@ export const Resume = () => {
             <div style={{ textAlign: "left" }}>
               <span>Software Engineer Intern • PowerSchool</span>
               <p>New York, NY</p>
-              <p>Desc</p>
-              <p>Tech stack i circular blocks</p>
-              <p>Make each block clickable (send them to company site)</p>
+              <ul>
+                <li>Developed an internal tool for managing student data.</li>
+                <li>
+                  Extract and migrated data from XML documents containing 25-200
+                  database tables for each of the 50 states.
+                </li>
+                <li>
+                  Decreased the time needed to manage those database tables by
+                  50%.
+                </li>
+                <li>
+                  Utilized Java library’s DOM parser to parse and extract data
+                  from documents, and the JSOUP library to parse HTML text.
+                </li>
+                <li>
+                  Created ~6 custom Java objects and ~15 regex patterns to match
+                  specific XML elements and attributes.
+                </li>
+                <li>
+                  Performed thorough testing utilizing virtual machines to
+                  ensure 100% accuracy and integrity of data.
+                </li>
+                <li>
+                  Improved the user experience for 70+ developers by
+                  implementing a GUI dialog and converting the program into a
+                  jar file.
+                </li>
+                <li>
+                  Delivered project demo to 5 members of the engineering
+                  leadership, accompanied by detailed technical documentation.
+                </li>
+              </ul>
+              <ul className="technologies-used">
+                <li>Java</li>
+                <li>Jira</li>
+                <li>Confluence</li>
+                <li>XML</li>
+                <li>JSOUP</li>
+              </ul>
             </div>
           </div>
         </li>
 
-        <li>
+        <li className="job-experience">
           <div
             style={{
               display: "flex",
