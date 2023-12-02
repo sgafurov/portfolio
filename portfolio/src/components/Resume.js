9533e4e47debc06890f08f000263364303c0e0d8
Shakhram <shakag6@gmail.com>
2023-12-02 11:42:28 -0500

    Add more content to Resume and edit css

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -3,32 +3,25 @@ import "../styles/Resume.css";
 export const Resume = () => {
   return (
     <div className="resume">
+      <h1>Experiences</h1>
       <ol>
-        <li className="job-experience">
-          <div
-            style={{
-              display: "flex",
-              alignContent: "center",
-              margin: "0 auto",
-              justifyContent: "space-between",
-            }}
-          >
-            <div>
-              <p style={{ margin: "0px" }}>June 2022 - May 2023</p>
+        <li className="experiences">
+          <div className="dates-and-descriptions">
+            <div className="job-dates">
+              <p>June 2022 - May 2023</p>
             </div>
-            <div style={{ textAlign: "left" }}>
-              <span>Software Engineer Intern • PowerSchool</span>
+            <div className="job-descriptions">
+              <span>
+                <strong>Software Engineer Intern • PowerSchool</strong>
+              </span>
               <p>New York, NY</p>
               <ul>
                 <li>Developed an internal tool for managing student data.</li>
                 <li>
-                  Extract and migrated data from XML documents containing 25-200
-                  database tables for each of the 50 states.
+                  Extracted and migrated data from XML documents containing
+                  25-200 database tables for each of the 50 states.
                 </li>
-                <li>
-                  Decreased the time needed to manage those database tables by
-                  50%.
-                </li>
+                <li>Decreased time needed to manage database tables by 50%.</li>
                 <li>
                   Utilized Java library’s DOM parser to parse and extract data
                   from documents, and the JSOUP library to parse HTML text.
@@ -42,9 +35,8 @@ export const Resume = () => {
                   ensure 100% accuracy and integrity of data.
                 </li>
                 <li>
-                  Improved the user experience for 70+ developers by
-                  implementing a GUI dialog and converting the program into a
-                  jar file.
+                  Improved user experience for 70+ developers by implementing a
+                  GUI dialog and converting the program into a jar file.
                 </li>
                 <li>
                   Delivered project demo to 5 members of the engineering
@@ -62,24 +54,43 @@ export const Resume = () => {
           </div>
         </li>
 
-        <li className="job-experience">
-          <div
-            style={{
-              display: "flex",
-              alignContent: "center",
-              margin: "0 auto",
-              justifyContent: "space-between",
-            }}
-          >
-            <div>
-              <p style={{ margin: "0px" }}>Jan 2022 - Aug 2022</p>
+        <li className="experiences">
+          <div className="dates-and-descriptions">
+            <div className="job-dates">
+              <p>Jan 2022 - Aug 2022</p>
             </div>
-            <div style={{ textAlign: "left" }}>
-              <span>Fullstack Developer Fellow • Tech Talent Pipeline</span>
+            <div className="job-descriptions">
+              <span>
+                <strong>
+                  Fullstack Developer Fellow • Tech Talent Pipeline
+                </strong>
+              </span>
               <p>Brooklyn, NY</p>
-              <p>Desc</p>
-              <p>Tech stack i circular blocks</p>
-              <p>Make each block clickable (send them to company site)</p>
+              <ul>
+                <li>
+                  Acquired hands-on training in full-stack web development by
+                  learning a comprehensive range of in-demand technologies.
+                </li>
+                <li>
+                  Completed 2-3 project-based assignments per week with projects
+                  ranging between designing wireframes with Figma, managing
+                  databases using Postgres, or creating full stack websites with
+                  Javascript, React, Node, and Express.
+                </li>
+                <li>
+                  Managed a team of 3 people in a capstone project to develop
+                  and deliver a full stack web application in 4 days.
+                </li>
+              </ul>
+              <ul className="technologies-used">
+                <li>Javascript</li>
+                <li>React</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>Postgres</li>
+              </ul>
             </div>
           </div>
         </li>
